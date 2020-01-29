b50630b5f08743a7f1a8de62413edd40a27a2d24
Jeff Oliver <jeffoliver3384@gmail.com>
2020-01-29 17:33:19 -0500

    run user migration

--- data/migrations/20200116165512_users.js
+++ data/migrations/20200116165512_users.js
@@ -1,17 +1,18 @@
-
 exports.up = function(knex) {
-    return knex.schema
-    .createTable('users', t => {
-        t.increments('user_id')
-        t.string('username', 255)
-        .notNullable().unique()
-        t.string('password', 255).notNullable()
-    })
-  
+  return knex.schema.createTable("users", t => {
+    t.increments();
+    t.string("uid", 255)
+      .unique()
+      .notNullable();
+    t.string("email", 255)
+      .unique()
+      .notNullable();
+    t.string("displayname", 255).unique();
+    t.stirng("firstname", 255);
+    t.string("lastname", 255);
+  });
 };
 
 exports.down = function(knex) {
-    return knex.schema
-    .dropTableIfExists('users');
-  
+  return knex.schema.dropTableIfExists("users");
 };
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
 module.exports = {
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/cryptoUsers"
-    },
+    client: "pg",
+    connection:
+      "postgres://lgyzosthkchxiz:980c097f66f595ccd6f897fef0033c0be33e6feb0c134b6c0d3e02bad1400a39@ec2-107-21-201-238.compute-1.amazonaws.com:5432/d3m0vpf3mmptbb",
     migrations: {
       directory: "./data/migrations"
     },
